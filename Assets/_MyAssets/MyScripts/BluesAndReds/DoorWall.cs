5ed81c8a810b6ad30002075851aabb9044c2cf4b
temitinho <artemiofonseca@gmail.com>
2018-06-03 19:38:46 -0300

    deleted non used files
    deleted non used files

--- Assets/_MyAssets/MyScripts/BluesAndReds/DoorWall.cs
+++ Assets/_MyAssets/MyScripts/BluesAndReds/DoorWall.cs
@@ -20,28 +20,28 @@ public class DoorWall : MonoBehaviour {
 	void Update () 
 	{
 
-        //Vector3 mousePos = Input.mousePosition;
-        //mousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x,mousePos.y,cam.transform.position.z - transform.position.z));
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.transform.position.z - transform.position.z));
 
-        //playerPos = new Vector3 (14, 0, Mathf.Clamp(mousePos.z, -11.5f, 11.5f));
-        //transform.position = playerPos;
-        if (Input.touchCount == 1)
+        playerPos = new Vector3(14, 0, Mathf.Clamp(mousePos.z, -11.5f, 11.5f));
+        transform.position = playerPos;
+        //if (Input.touchCount == 1)
 
-        {
-            //transform.Translate (Input.touches [0].deltaPosition.x * .25f, Input.touches [0].deltaPosition.y * .25f, 0); 
+        //{
+        //transform.Translate (Input.touches [0].deltaPosition.x * .25f, Input.touches [0].deltaPosition.y * .25f, 0); 
 
-            Touch touch = Input.GetTouch(0);
+        //Touch touch = Input.GetTouch(0);
 
-            //float x = -8f + 16 * touch.position.x / Screen.width;
-            float y = -14f + 28 * touch.position.y / Screen.height;
-            //Vector3 pos = Input.mousePosition;
-            //pos = cam.ScreenToWorldPoint(new Vector3(x, y, cam.transform.position.z - transform.position.z));
+        //float x = -8f + 16 * touch.position.x / Screen.width;
+        //float y = -14f + 28 * touch.position.y / Screen.height;
+        //Vector3 pos = Input.mousePosition;
+        //pos = cam.ScreenToWorldPoint(new Vector3(x, y, cam.transform.position.z - transform.position.z));
 
-            //playerPos = new Vector3(14, 0, Mathf.Clamp(mousePos.z, -11.5f, 11.5f));
-            //transform.position = playerPos;
+        playerPos = new Vector3(14, 0, Mathf.Clamp(mousePos.z, -11.5f, 11.5f));
+        transform.position = playerPos;
 
-            transform.position = new Vector3(14, 0, y);
+        //transform.position = new Vector3(14, 0, y);
 
-        }
+        //}
     }
 }
